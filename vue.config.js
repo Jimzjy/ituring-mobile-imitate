c60c7cd431b252161b2c825491b27edbfd2526e3
Jimzjy <jimzjy@gmail.com>
2019-05-31 13:34:24 +0800

    build preview

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   transpileDependencies: [
     /\bvue-awesome\b/
-  ]
+  ],
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production' ? '/ituring-mobile-imitate/' : '/'
 }
